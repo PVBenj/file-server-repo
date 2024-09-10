c5d1aa02cc7795c295304a832efdb7e72a810951
Benjamin <pramodyabenjamin@gmail.com>
2024-09-10 22:18:47 +0530

    Login, Home page UI committed

---
+++ Client/src/main/java/View/Home/UIMethods.java
@@ -0,0 +1,9 @@
+package View.Home;
+
+import java.awt.Color;
+import javax.swing.JPanel;
+
+
+public interface UIMethods {
+    void changeColor(JPanel hover, Color myColor);
+}
---
+++ Client/src/main/java/View/Login/LoginInterface.java
@@ -0,0 +1,9 @@
+package View.Login;
+
+import java.rmi.Remote;
+
+
+public interface LoginInterface extends Remote{
+    //Define abstract methods for login
+    
+}
